a65df644ba3a9e9b9d809aea4bd03065cb674bca
Amith <amithajith7025@gmail.com>
2025-12-27 07:59:33 +0530

    I just stepped in

---
+++ hello-world/target/debug/deps/hello_world-c3ef83e6baa092c1.d
@@ -0,0 +1,5 @@
+/mnt/stone/Rust-Programming-Language/hello-world/target/debug/deps/hello_world-c3ef83e6baa092c1.d: src/main.rs
+
+/mnt/stone/Rust-Programming-Language/hello-world/target/debug/deps/hello_world-c3ef83e6baa092c1: src/main.rs
+
+src/main.rs:
---
+++ hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/mnt/stone/Rust-Programming-Language/hello-world/target/debug/hello-world: /mnt/stone/Rust-Programming-Language/hello-world/src/main.rs
