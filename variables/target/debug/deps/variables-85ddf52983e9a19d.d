a816e55231b04a0cc82bbd0e9051686185b50dd0
Amith <amithajith7025@gmail.com>
2025-12-27 08:07:22 +0530

    created my first variable

---
+++ variables/target/debug/deps/variables-85ddf52983e9a19d.d
@@ -0,0 +1,5 @@
+/mnt/stone/Rust-Programming-Language/variables/target/debug/deps/variables-85ddf52983e9a19d.d: src/main.rs
+
+/mnt/stone/Rust-Programming-Language/variables/target/debug/deps/variables-85ddf52983e9a19d: src/main.rs
+
+src/main.rs:
